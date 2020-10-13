269600016594662ce447ba69390bc6719547e78a
Adrian Georgescu <ag@ag-projects.com>
2020-10-13 13:57:52 +0000

    Sync internal version number

--- gnutls/__info__.py
+++ gnutls/__info__.py
@@ -6,7 +6,7 @@ __project__ = "python-gnutls"
 __summary__ = "Python wrapper for the GnuTLS library"
 __webpage__ = "https://github.com/AGProjects/python3-gnutls"
 
-__version__ = "3.1.4"
+__version__ = "3.1.7"
 
 __author__ = "Dan Pascu"
 __email__ = "support@ag-projects.com"
