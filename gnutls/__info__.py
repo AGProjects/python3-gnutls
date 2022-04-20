3abe688e1a7590fae442cddbeca09f9d29ec52bc
Tijmen de Mes <tijmen@ag-projects.com>
2022-04-20 07:38:14 +0000

    Raised version and updated changelog

--- gnutls/__info__.py
+++ gnutls/__info__.py
@@ -6,10 +6,10 @@ __project__ = "python3-gnutls"
 __summary__ = "Python wrapper for the GnuTLS library"
 __webpage__ = "https://github.com/AGProjects/python3-gnutls"
 
-__version__ = "3.1.9"
+__version__ = "3.1.10"
 
 __author__ = "Dan Pascu"
 __email__ = "support@ag-projects.com"
 
 __license__ = "LGPL"
-__copyright__ = "Copyright 2006-2021 {}".format(__author__)
+__copyright__ = "Copyright 2006-2022 {}".format(__author__)
